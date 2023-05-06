957c49979ccd2fd5377dfd790d1f165961fd37d9
Philipp Eder <philipp.eder@posteo.net>
2023-05-06 08:10:45 +0200

    Preparing for multiple machines

--- modules/default.nix
+++ modules/default.nix
@@ -1,22 +1,21 @@
 { inputs, nixpkgs, self, ... }:
 
 let
-  system = "x86_64-linux";
   pkgs = import nixpkgs {
-    inherit system;
     config.allowUnfree = true;
   };
   lib = nixpkgs.lib;
 in
 {
-  nixos = nixpkgs.lib.nixosSystem {
+  denkspatz = nixpkgs.lib.nixosSystem {
     specialArgs = { inherit self inputs; };
+    # TODO move all but denkspatz to an own module
     modules =
       [ (import ./bootloader.nix) ] ++
       [ (import ./misc.nix) ] ++
       [ (import ./fish.nix) ] ++
       [ (import ./fonts.nix) ] ++
       [ (import ./philipp.nix ) ] ++
-      [ (import ./../hardware-configuration.nix) ];
+      [ (import ./../denkspatz.nix) ];
   };
 }

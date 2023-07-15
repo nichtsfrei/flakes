632b3fe6a2541383660c4686414e6f9d6c8ab834
Philipp Eder <philipp.eder@posteo.net>
2023-07-15 10:55:02 +0200

    Add paneedit.sh as a command a+h
    To paste the pane history to a file and popup a window to edit it a+h is
    added.

--- modules/tmux/config/paneedit.sh
+++ modules/tmux/config/paneedit.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+width=${2:-80%}
+height=${2:-80%}
 FILENAME=$(mktemp /tmp/paneedit.XXXXXX)
 tmux capture-pane -pS - > "$FILENAME"
 EDITOR=${EDITOR:-nvim}
-$EDITOR "$FILENAME"
+tmux popup -xC -yC -w"$width" -h"$height" -E "$EDITOR" "$FILENAME"

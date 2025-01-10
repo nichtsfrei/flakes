ec4696dd8985efd23445ead3817588cdaddaa1e9
Philipp Eder <philipp.eder@posteo.net>
2025-01-10 11:53:49 +0100

    Fix: endless select on probing, removing omp

--- read-kb-layer/daemon.c
+++ read-kb-layer/daemon.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <fcntl.h>
@@ -11,14 +12,14 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <dirent.h>
-#include <omp.h>
+#include <pthread.h>
 
 #define HIDRAW_DIR "/dev"
 
 
 struct LayeredKeyboard{
     int fd;
-    const char *path;
+    char *path;
     char layer;
 };
 
@@ -29,40 +30,50 @@ struct EitherLayered {
     char layer;
 };
 
-struct EitherLayered is_layer_device(char* device_path) {
+void *probe_device(void *out) {
+    assert(out != NULL);
+    struct EitherLayered *result = out;
     unsigned char data[32] = {0};
     data[0] = 'S';
     struct timeval timeout = { 1, 0};
-    int fd = open(device_path, O_RDWR);
-    struct EitherLayered result = {device_path, fd, -1, -1};
-    if (fd == -1) {
-        return result;
+    if (result->path == NULL) {
+        result->result = -2;
+        return NULL;
     }
-    int res = write(fd, data, sizeof(data));
+
+    // indicates issues that have set errno
+    result->result = -1;
+    result->fd = open(result->path, O_RDWR);
+    if (result->fd == -1) {
+        goto end;
+    }
+    int res = write(result->fd, data, sizeof(data));
     if (res == -1) {
-        return result;
+        goto end;
     }
     
     fd_set read_fds;
     FD_ZERO(&read_fds);
-    FD_SET(fd, &read_fds);
-    res = select(fd + 1, &read_fds, NULL, NULL, &timeout);
-    if (res == -1) {
-        return result;
+    FD_SET(result->fd, &read_fds);
+    res = select(result->fd + 1, &read_fds, NULL, NULL, &timeout);
+    if (res <= -1) {
+        goto end;
     } else if (res == 0) {
-        result.result = -2;
-    } else {
-        res = read(fd, data, sizeof(data));
-        if (res == -1) {
-            result.result = -3;
-        } else if (data[0] == 'S' && data[1] == 1) {
-            result.result = 1;
-            result.layer = data[31];
-        } else {
-            result.result = 0;
-        }
+        result->result = 0;
+        goto end;
     }
-    return result;
+    
+    res = read(result->fd, data, sizeof(data));
+    if (res > 0 && data[0] == 'S' && data[1] == 1) {
+        result->result = 1;
+        result->layer = data[31];
+    } else {
+        result->result = 0;
+    }
+end:
+    printf("probed %s\tresult: %3i\n", result->path, result->result);
+    return NULL;
+    
 }
 
 
@@ -77,14 +88,12 @@ struct LayeredKeyboards probe_devices() {
     size_t hd = strlen(HIDRAW_DIR);
     size_t devices = 0, device_cap = 20, name_len, layerd_devices = 0;
     char **device_paths = calloc(device_cap, sizeof(*device_paths));
-
     
     if (dir == NULL) {
         perror("Failed to open /dev directory");
         free(device_paths);
         exit(1);
     }
-
 
     while ((entry = readdir(dir)) != NULL) {
         if (strncmp(entry->d_name, "hidraw", 6) == 0) {
@@ -101,15 +110,18 @@ struct LayeredKeyboards probe_devices() {
     }
     closedir(dir);
     struct EitherLayered layered[devices];
-    #pragma omp parallel for
-    for (int i = 0; i < devices; ++i){
-        printf("%u => %s\n", i, device_paths[i]);
-        layered[i] = is_layer_device(device_paths[i]);
+    pthread_t threads[devices]; 
+
+    for (size_t i = 0; i < devices; ++i){
+        layered[i].path = device_paths[i];
+        pthread_create(&threads[i], NULL, probe_device, (void*) &layered[i]);
+    }
+ 
+    for (size_t i = 0; i < devices; ++i){
+        pthread_join(threads[i], NULL);
     }
 
-    
-
-    for (int i = 0; i < devices; ++i){
+    for (size_t i = 0; i < devices; ++i){
         if (layered[i].result == 1) {
             layerd_devices += 1;
         } else {
@@ -121,7 +133,7 @@ struct LayeredKeyboards probe_devices() {
     struct LayeredKeyboards result;
     result.len = layerd_devices;
     result.kbs = malloc(result.len * sizeof(*result.kbs));
-    for (int i = 0; i < devices; ++i){
+    for (size_t i = 0; i < devices; ++i){
         if (layered[i].result == 1) {
             result.kbs[ri].fd = layered[i].fd;
             result.kbs[ri].path = layered[i].path;
@@ -150,12 +162,11 @@ static int read_hid(struct LayeredKeyboard *kb) {
     }
 }
 
-int main() {
-    char layer;
+int main(int argc, char *argv[]) {
     int res;
     char data[32] = {0};
     struct sockaddr_un addr;
-    const char *socket_path = "/tmp/rofl.sock";
+    const char *socket_path = argc == 2 ? argv[1] : "/tmp/rofl.sock";
     struct LayeredKeyboards kbs = probe_devices();
     struct pollfd fds[kbs.len + 1];
 
@@ -183,22 +194,20 @@ int main() {
         perror("Failed to listen on socket");
         goto result;
     }
-
+    printf("listening on %s\n", socket_path);
     for (int i = 0; i < kbs.len; ++i) {
         fds[i + 1].fd = kbs.kbs[i].fd;
         fds[i + 1].events = POLLIN;
     }
-    
-    printf("Layer: %u\n", layer);
-    
+ 
     while (1) {
         res = poll(fds, kbs.len + 1, -1);
         if (res == -1) {
-            perror("poll failed\n");
+            perror("poll failed");
             break;
         }
         if (fds[0].revents & POLLIN) {
-            int client_fd = accept(fds[1].fd, NULL, NULL);
+            int client_fd = accept(fds[0].fd, NULL, NULL);
             if (client_fd == -1) {
                 perror("Failed to accept Unix socket connection");
                 continue;
@@ -213,21 +222,25 @@ int main() {
 
             if (data[0] == 'S') {
                 char answer[1024] = {0};
-                // TODO: combine each ...
-                int size = snprintf(answer, 1024, "%u\n", layer);
-                
-                res = write(client_fd, answer, size);
-                if (res == -1) {
-                    perror("Failed to send state to Unix socket client");
+                char *header =  "device\tlayer\n";
+                size_t header_len =  strlen(header);
+                if (write(client_fd, header, header_len) == -1) {
+                   perror("Failed to send header to client");
+                } else {
+                   for (size_t i = 0; i < kbs.len; ++i) {
+                        int size = snprintf(answer, 1024, "%s\t%u\n", kbs.kbs[i].path, kbs.kbs[i].layer);
+                        res = write(client_fd, answer, size);
+                        if (res == -1) {
+                            perror("Failed to send state to client");
+                        }
+                   }
                 }
-                printf("Sent layer state response to Unix socket client.\n");
-            } 
-
-            close(client_fd);
+           } 
+           close(client_fd);
         }
         for (int i = 0; i < kbs.len; ++i) {
             if (fds[i + 1].revents & POLLIN && read_hid(&kbs.kbs[i]) == -1){
-                perror("unable to read from hid device\n");
+                perror("unable to read from hid device");
                 res = -3;
                 break;
             }
@@ -235,9 +248,11 @@ int main() {
 
     }
 result:
-    // TODO: cleanup
-    for (int i = 0; i < kbs.len + 1; i++) {
-        close(fds[i].fd);
+    close(fds[0].fd);
+    for (int i = 0; i < kbs.len; i++) {
+        close(fds[i + 1].fd);
+        free(kbs.kbs[i].path);
     }
+    free(kbs.kbs);
     return res;
 }
